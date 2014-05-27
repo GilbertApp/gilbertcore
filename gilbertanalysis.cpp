860863536e15f06359d30d8a07b2a83f4eb49e5a
Hugh Rawlinson <hugh@codeoclock.net>
2014-05-27 21:34:37 +0400

    asd

--- gilbertanalysis.cpp
+++ gilbertanalysis.cpp
@@ -43,50 +43,6 @@ double gilbertanalysis::calcSC(std::vector<double>& buffer){
 }
 
 //---------------------------------------------------------------
-std::vector<double> gilbertanalysis::getExactHit(std::vector<double> &hitBuffer, double threshold){
-
-    //Every how many samples should the RMS be calculated.
-    int resolution = 100;
-    //Initialisation of the hightest RMS bin.
-    int highestRMSBin = 0;
-    //A new vector that will contain the exact hit.
-    int exactHitLength = 4096;
-    std::vector<double> exactHit(exactHitLength);
-    //An array of doubles, which each representing the RMS of 100 samples.
-    std::vector<double> rmsInEachBin;
-    //Setting the threshold.
-    double highestRMSValue = threshold;
-
-    for(int i = 0; i<hitBuffer.size(); i+=resolution){
-        //create small vectors, each of length 100 samples.
-        std::vector<double> hitBufferBin(&hitBuffer[i],&hitBuffer[i+resolution]);
-
-        //Storing RMS of every 100 samples in a vector.
-        rmsInEachBin.push_back(calcRMS(hitBufferBin));
-    }
-
-    //Checking whether the RMS is greater then the average RMS - if so, detect an onset.
-    //Only the highest RMS value is kept, therefore, only the loudest hit is kept.
-    for (int i = 0; i < rmsInEachBin.size(); i++){
-        // std::cout<<rmsInEachBin.at(i)<<std::endl;
-        if(rmsInEachBin.at(i)>highestRMSValue){
-            // std::cout << i<<std::endl;
-            highestRMSValue = rmsInEachBin.at(i);
-            highestRMSBin = i*resolution;
-        }
-    }
-
-    for(int j = 0 ; j < exactHit.size(); j++) {
-        // populates a new vector with the values of the exact hit.
-        //This would crash with an array out of bounds if the hit was done at the end of the two seconds.
-        exactHit.at(j)=hitBuffer.at(j+highestRMSBin);
-    }
-
-    //returns ~200 milliseconds of the exact hit.
-    return exactHit;
-}
-
-//---------------------------------------------------------------
 sfs gilbertanalysis::analyseHitBuffer(std::vector<double> &exactHitBuffer, std::string drum){
 
     int windowSize = 128;
@@ -127,17 +83,6 @@ sfs gilbertanalysis::analyseHitBuffer(std::vector<double> &exactHitBuffer){
     sfs hitInfo = createSFS(centroidEnvelope, rmsEnvelope);
     
     return hitInfo;
-}
-//---------------------------------------------------------------
-sfs gilbertanalysis::createSFS(std::vector<double> centroids, std::vector<double> rmss, std::string drum){
-
-    sfs hitInfo = {.id = drum, 
-                   .sc_mean = calcMean(centroids), .sc_stanDev = calcStanDev(centroids), .sc_min = getMin(centroids), .sc_max = getMax(centroids),
-                   .rms_mean = calcMean(rmss), .rms_stanDev = calcStanDev(rmss), .rms_min = getMin(rmss), .rms_max = getMax(rmss)
-                };
-
-    return hitInfo;
-
 }
 
 //---------------------------------------------------------------
@@ -198,30 +143,3 @@ double gilbertanalysis::getMax(std::vector<double> v){
 }
 
 /******************************************************************************/
-
-
-//--------------------------------------------------------------
-void gilbertanalysis::writeWAV(std::vector<double>& buffer, std::string drum, sfs info){
-    float* exactHitArray;
-    exactHitArray = new float[buffer.size()];
-    for(int j = 0; j<buffer.size(); j++){
-        exactHitArray[j] = buffer[j];
-    }
-    // define the desired output format
-    SF_INFO sfinfo ;
-    sfinfo.channels = 1;
-    sfinfo.samplerate = 44100;
-    sfinfo.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
-    
-    std::string path = "";
-    path+=drum;
-    path+=".wav";
-    std::cout << path.c_str() << std::endl;
-    
-    SNDFILE * outfile = sf_open(path.c_str(), SFM_WRITE, &sfinfo);
-    char strbuf[50];
-    sf_set_string(outfile, SF_STR_COMMENT, strbuf);
-    sf_count_t count = sf_write_float(outfile, &exactHitArray[0], buffer.size());
-    sf_write_sync(outfile);
-    sf_close(outfile);
-}

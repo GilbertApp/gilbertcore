0dc1b91095f08eb12800eacba4d1fbac62f56c76
Hugh Rawlinson <hugh@codeoclock.net>
2014-06-08 14:17:16 +0100

    Update gilbert.h
    Added documentation

--- gilbert.h
+++ gilbert.h
@@ -1,3 +1,6 @@
+//! An interface to the gilbert core
+
+//! Provides high level gilbert functionality to a platform specific wrapper.
 #include <stdio.h>
 #include <fftw3.h>
 #include <sndfile.h>
@@ -15,14 +18,22 @@ class gilbert{
     
 
 public:
+    //! Constructor
     gilbert();
+    //! Destructor
     ~gilbert();
     
+    //! provides the audio to gilbert core
+    //! @param buffer an audio buffer
+    //! @return a string representing the looked up drum
     std::string audioIn(std::vector<double> buffer);
-    //analysing the hit and adding it to the database.
+    //! sets a flag for the 'current' buffer to be added to the database under the name
+    //! @param name the name of the drum
     void registerBuffer(std::string name);
-
+    //! deletes the last buffer that was saved into the database
     void deleteLastBuffer();
+    //! deletes a specific sfs in the database by id
+    //! the id of the sfs to be deleted
     void deleteBufferByID(std::string id);
 
 };
